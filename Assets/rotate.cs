997f2a363d0352081f520ae6a1bc9729474fe907
Ananda Poudel <apoudel@stcloudstate.edu>
2017-11-02 13:34:47 -0500

    Working Fine

--- Assets/rotate.cs
+++ Assets/rotate.cs
@@ -6,14 +6,21 @@ public class rotate : MonoBehaviour
     // Use this for initialization
     private GameObject colaCan;
     public Camera firstPersonCamera;
-    private bool firsttime = true;
-    public float timeLeft = 5;
+    private bool shootEnable = false;
+    public float timeLeft = 3;
     public GameObject Can;
     Rigidbody rb;
 
+    private Vector2 touchStart;
+    private Vector2 touchEnd;
+    private float canVelocity = 0;
+    private float canSpeed = 0;
+    private Vector3 worldAngle;
+    private bool couldBeSwipe = false;
+     
+
     void Start()
     {
-
     }
 
     /*
@@ -24,20 +31,24 @@ public class rotate : MonoBehaviour
     }
     */
     // Update is called once per frame
+
+
     void Update()
     {
         timeLeft -= Time.deltaTime;
         if (Input.touchCount > 0 && timeLeft < 0)
         {
-            timeLeft = 5;
             RaycastHit myHit;
             Touch myTouch = Input.GetTouch(0);
             Ray ray = firstPersonCamera.ScreenPointToRay(myTouch.position);
             if(Physics.Raycast(ray,out myHit, Mathf.Infinity))
-            if (firsttime)
-            {
+                    {
+                timeLeft = 3;
+                       
+                print(myHit.point);
+                print(myHit.point.y + 20);
 
-                colaCan = Instantiate(Can,myHit.point,Quaternion.identity);
+                colaCan = Instantiate(Can,new Vector3(myHit.point.x,myHit.point.y+20,myHit.point.z), Quaternion.identity);
                 colaCan.AddComponent<Rigidbody>();
                 colaCan.AddComponent<BoxCollider>();
                 colaCan.AddComponent<destroyAfterCertainSeconds>();
@@ -45,13 +56,27 @@ public class rotate : MonoBehaviour
                 rb = colaCan.GetComponent<Rigidbody>();
                 rb.useGravity = true;
                 rb.isKinematic = false;
+                rb.mass = 0.1f;
+                rb.AddForce(firstPersonCamera.transform.forward * 200, ForceMode.Acceleration);
+            }
+            /*
+            colaCan = Instantiate(Can,myHit.point,Quaternion.identity);
+            colaCan.AddComponent<Rigidbody>();
+            colaCan.AddComponent<BoxCollider>();
+            colaCan.AddComponent<destroyAfterCertainSeconds>();
 
-                rb.AddForce(transform.forward * 1, ForceMode.Impulse);
+            rb = colaCan.GetComponent<Rigidbody>();
+            rb.useGravity = true;
+            rb.isKinematic = false;
 
-                }
-            
-          
+            rb.AddForce(transform.forward * 1, ForceMode.Impulse);
+            */
+
+
+
+
         }
 
     }
+
 }
